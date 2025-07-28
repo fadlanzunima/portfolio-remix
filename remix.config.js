594849313a616ff7b9b03ca475c771fb1495bf5e
Fadlan Zunima <fadlanzunima@gmail.com>
2025-07-28 07:23:15 +0700

    .

--- remix.config.js
+++ remix.config.js
@@ -3,4 +3,13 @@ export default {
   ignoredRouteFiles: ["**/.*"],
   tailwind: true,
   postcss: true,
+  serverBuildPath: "functions/[[path]].js",
+  publicPath: "/",
+  assetsBuildDirectory: "public/_static",
+  serverConditions: ["workerd", "worker", "browser"],
+  serverDependenciesToBundle: "all",
+  serverMainFields: ["browser", "module", "main"],
+  serverMinify: true,
+  serverModuleFormat: "esm",
+  serverPlatform: "neutral",
 };
